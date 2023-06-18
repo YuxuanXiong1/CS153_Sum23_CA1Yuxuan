647ec3147bc15ca9764b0d6e82170d9c3fd7a61f
Yuxuan Xiong <yuxuanxiong@brandeis.edu>
2023-06-18 23:25:31 +0900

    13123
    2131

--- CA1/components/TodoList.js
+++ CA1/components/TodoList.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { View, Button, Text, TextInput, FlatList, TouchableOpacity } from 'react-native';
+import { View, Button, Text, TextInput, FlatList, TouchableOpacity, Picker } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const App = () => {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
+  const [importanceLevel, setImportanceLevel] = useState("1");
 
   useEffect(() => {
     readData()
@@ -31,10 +32,11 @@ const App = () => {
 
   const addTodo = () => {
     if (newTodo.length > 0) {
-      const updatedTodos = [...todos, { name: newTodo, completed: false }];
+      const updatedTodos = [...todos, { name: newTodo, completed: false, importance: importanceLevel }];
       setTodos(updatedTodos);
       storeData(updatedTodos);
       setNewTodo("");
+      setImportanceLevel("1");
     }
   };
 
@@ -55,6 +57,19 @@ const App = () => {
         value={newTodo}
         placeholder="Enter new todo"
       />
+
+      <Picker
+        selectedValue={importanceLevel}
+        style={{height: 50, width: 100}}
+        onValueChange={(itemValue, itemIndex) =>
+          setImportanceLevel(itemValue)
+        }>
+        <Picker.Item label="1" value="1" />
+        <Picker.Item label="2" value="2" />
+        <Picker.Item label="3" value="3" />
+        <Picker.Item label="4" value="4" />
+        <Picker.Item label="5" value="5" />
+      </Picker>
       
       <Button 
         title="Add Todo"
@@ -67,6 +82,7 @@ const App = () => {
           <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 30 }}>
             <Text style={{ fontSize: 20, marginRight: 10 }}>{item.name}</Text>
             <Text style={{ marginRight: 10 }}>{item.completed ? 'Completed' : 'Not Completed'}</Text>
+            <Text style={{ marginRight: 10 }}>Importance: {item.importance}</Text>
             <TouchableOpacity onPress={() => deleteTodo(index)}>
               <Text style={{ color: 'red' }}>Delete</Text>
             </TouchableOpacity>

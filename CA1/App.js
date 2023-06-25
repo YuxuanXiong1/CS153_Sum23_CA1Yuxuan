21a93297e3edb358bc753a43f6471501cfac17d2
Yuxuan Xiong <yuxuanxiong@brandeis.edu>
2023-06-26 00:54:55 +0900

    123

--- CA1/App.js
+++ CA1/App.js
@@ -1,15 +1,60 @@
-import React from 'react';
-import {View,Text} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View} from "react-native";
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
-import KeyPad from './components/Home';
 
-const App = () => {
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+
+const PlaceholderImage = require("./assets/images/background-image.png");
+
+export default function App() {
+
+    const pickImageAsync = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          allowsEditing: true,
+          quality: 1,
+        });
+    
+        if (!result.canceled) {
+          console.log(result);
+        } else {
+          alert('You did not select any image.');
+        }
+      };
+
     return (
-        <View style={{flex:1}}>
-            <Text style={{textAlign:'center',fontSize:40}}>FinalProject base</Text>
-            <KeyPad/>
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <ImageViewer placeholderImageSource={PlaceholderImage} />
         </View>
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+          <Button label="Use this photo" />
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 
-    )
-}
-export default App
+const styles = StyleSheet.create({
+    footerContainer: {
+        flex: 1 / 3,
+        alignItems: 'center',
+      },
+    container: {
+      flex: 1,
+      backgroundColor: '#25292e',
+      alignItems: 'center',
+    },
+    imageContainer: {
+      flex: 1,
+      paddingTop: 58,
+    },
+    image: {
+      width: 320,
+      height: 440,
+      borderRadius: 18,
+    },
+  });

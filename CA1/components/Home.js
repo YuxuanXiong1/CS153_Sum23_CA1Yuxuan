e85fbdb08d06c9c1c0b88c8154df1c1b4306a281
Yuxuan Xiong <yuxuanxiong@brandeis.edu>
2023-06-26 01:23:09 +0900

    changes

--- CA1/components/Home.js
+++ CA1/components/Home.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Todo from './TodoList';
 import Chat from './ChatGPTDemo';
+import Profile from './Profile';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,6 +14,7 @@ const App = () => {
       <Tab.Navigator>
         <Tab.Screen name="Todo" component={Todo}/>
         <Tab.Screen name="Chat" component={Chat}/>
+        <Tab.Screen name="Profile" component={Profile}/>
       </Tab.Navigator>
     </NavigationContainer>
   );

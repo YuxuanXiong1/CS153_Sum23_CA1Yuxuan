70011c24403174a6e180456127385571b07ec7f6
Yuxuan Xiong <yuxuanxiong@brandeis.edu>
2023-07-04 00:33:41 +0900

    update
    for camera

--- .expo/README.md
+++ .expo/README.md
@@ -1,15 +1,52 @@
-> Why do I have a folder named ".expo" in my project?
+import React, { useState, useEffect } from 'react';
+import { Text, FlatList, View, Image, TextInput } from 'react-native';
+ 
 
-The ".expo" folder is created when an Expo project is started using "expo start" command.
+const Question2 = () => {
+    const [data,setData] = useState([]);
+    const [loading,setLoading] = useState(true);
+    const [ingredient, setIngredient] = useState("chicken_breast");
 
-> What do the files contain?
+    const getMeals = async () => {
+        try {
+          const url = "https://www.themealdb.com/api/json/v1/1/filter.php?i=" + ingredient;
+          const response = await fetch(url);
+          const json = await response.json();
+          if (json.meals) {
+            setData(json.meals);
+          } else {
+            setData([]);
+          }
+        } catch (error) {
+          console.error(error);
+        } finally {
+          setLoading(false);
+        }
+      };
 
-- "devices.json": contains information about devices that have recently opened this project. This is used to populate the "Development sessions" list in your development builds.
-- "packager-info.json": contains port numbers and process PIDs that are used to serve the application to the mobile device/simulator.
-- "settings.json": contains the server configuration that is used to serve the application manifest.
+    useEffect(() => {getMeals()}, [ingredient])
 
-> Should I commit the ".expo" folder?
+    return(
+        <View style={{padding:30}}>
+            <Text style={{fontSize: 60}}>Meal Finder</Text>
+            <Text style={{fontSize: 26}}>Main Ingredient: {ingredient} </Text>
+            <Text style={{fontSize: 26}}>Enter the new Ingredient:</Text>
+            <TextInput
+                style={{height: 40, borderWidth: 1}}
+                onChangeText={setIngredient}
+            />
+            <FlatList
+                data={data.slice(0,20)}
+                keyExtractor={({ idMeal }, index) => idMeal}
+                renderItem={({ item }) => (
+                    <Meal
+                        name={item.strMeal}
+                        image={item.strMealThumb}
+                    />
+                )}
+            />
+        </View>
+    );
+}
 
-No, you should not share the ".expo" folder. It does not contain any information that is relevant for other developers working on the project, it is specific to your machine.
-
-Upon project creation, the ".expo" folder is already added to your ".gitignore" file.
+export default Question2;
--- CA1/components/CameraScreen.js
+++ CA1/components/CameraScreen.js
@@ -1,10 +1,41 @@
-import React, { useState } from 'react';
-import { Button, View, Image, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Button, View, Image, StyleSheet, TextInput, FlatList, TouchableOpacity, Modal, Text } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { Camera } from 'expo-camera';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function CameraScreen() {
   const [photoUri, setPhotoUri] = useState(null);
+  const [photoName, setPhotoName] = useState('');
+  const [photoList, setPhotoList] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  useEffect(() => {
+    retrieveData();
+  }, []);
+
+  const retrieveData = async () => {
+    try {
+      const storedPhotoList = await AsyncStorage.getItem('photoList');
+      if (storedPhotoList !== null) {
+        setPhotoList(JSON.parse(storedPhotoList));
+      }
+    } catch (error) {
+      console.log('Failed to retrieve data from storage:', error);
+    }
+  };
+
+  const saveData = async (uri, name) => {
+    try {
+      const timestamp = new Date().toISOString();
+      const newList = photoList.concat({ uri, name, timestamp });
+      setPhotoList(newList);
+      await AsyncStorage.setItem('photoList', JSON.stringify(newList));
+    } catch (error) {
+      console.log('Failed to save data to storage:', error);
+    }
+  };
 
   const takePhoto = async () => {
     const { status } = await Camera.requestPermissionsAsync();
@@ -17,12 +48,52 @@ export default function CameraScreen() {
     if (!result.cancelled) {
       setPhotoUri(result.assets[0].uri);
     }
+  };
+
+  const savePhoto = () => {
+    if (photoUri && photoName) {
+      saveData(photoUri, photoName);
+      setPhotoUri(null);
+      setPhotoName('');
+    } else {
+      alert('Please take a photo and give it a name.');
+    }
   }
+
+  const ListItem = ({ item }) => (
+    <TouchableOpacity style={styles.listItem} onPress={() => { setSelectedItem(item); setModalVisible(true); }}>
+      <Text style={styles.text}>{item.name}</Text>
+      <Text style={styles.dateText}>{new Date(item.timestamp).toLocaleString()}</Text>
+    </TouchableOpacity>
+  );
 
   return (
     <View style={styles.container}>
       <Button title="Take Photo" onPress={takePhoto} />
       {photoUri && <Image source={{ uri: photoUri }} style={styles.image} />}
+      <TextInput
+        style={styles.input}
+        onChangeText={setPhotoName}
+        value={photoName}
+        placeholder='Enter photo name'
+      />
+      <Button title="Save Photo" onPress={savePhoto} />
+      <FlatList
+        data={photoList}
+        renderItem={ListItem}
+        keyExtractor={item => item.timestamp}
+      />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+      >
+        <View style={styles.modalView}>
+          <Image source={{ uri: selectedItem?.uri }} style={styles.image} />
+          <Text style={styles.modalText}>{selectedItem?.name}</Text>
+          <Button title="Close" onPress={() => setModalVisible(false)} />
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -37,5 +108,46 @@ const styles = StyleSheet.create({
     width: 300,
     height: 300,
     marginTop: 20,
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    margin: 12,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 10,
+    paddingHorizontal: 10,
+  },
+  listItem: {
+    backgroundColor: '#f8f8f8',
+    padding: 20,
+    borderRadius: 10,
+    marginVertical: 8,
+  },
+  text: {
+    fontSize: 16,
+  },
+  dateText: {
+    fontSize: 12,
+    color: '#666',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
   },
 });
